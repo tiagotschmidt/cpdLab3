adf293ef85ac0315a6b691fc17c5672bbfda08ad
Tiago Torres Schmidt <tiago.schmidt@inf.ufrgs.br>
2021-09-30 09:08:33 -0300

    Alteração de Lista Linear para matriz de alocação dinâmica. Função Radix Sort implementada. Restando apenas a contagem de repetição.

--- radixSort.c
+++ radixSort.c
@@ -5,13 +5,13 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <locale.h>
-#include "lista.h"
 
-#define MAX_ITEMS 60000//Constantes utilizadas no projeto.
+#define MAX_ITEMS 400000//Constantes utilizadas no projeto.
 #define MAX_CHAR 20
+#define R 256
 #define DEFAULT_INPUT "84-0.txt"
 #define DEFAULT_OUTPUT1 "fonte.txt"
-#define DEFAULT_OUTPUT2 "listaLinear.txt"
+#define DEFAULT_OUTPUT2 "fonteFINAL.txt"
 
 void prtInfo(int vectorLength, int swaps, int recursions, float time, FILE* output){//Função prtInfo. Printa informações requisitadas dentro do arquivo apontado por *output.
   fprintf(output,"TAMANHO ENTRADA %d\n",vectorLength);
@@ -28,30 +28,33 @@ void string_toLower (char *s)
     }
 }
 
-void indexWords(FILE* input, FILE* output1, FILE* output2, char array[MAX_CHAR][MAX_ITEMS]){
+char** indexWords(FILE* input, char** array, int* nItens){
   char line[200];
   char *word;
-  char separator[]= {" —’0123456789,.&*%%\?!;/-'@\"$#=~><()][}{:\b\r\a\7\\\"\'“”_"};
-  NodeItem* list;
+  char c;
+  int i;
+  char separator[]= {" —’0123456789,.&-‘*%%\?!;/-'@\"$#=~><()][}{:\b\r\a\7\\\"\'“”_"};
 
-  list = Inicializa_Lista();
+  i=0;
+  c=getc(input);
+  c=getc(input);
+  c=getc(input);
 
   while (fgets(line,200,input)){
     string_toLower(line);
     word = strtok(line, separator); //pega a 1a palavra do tweet
 
     while (word != NULL){
-      if(strlen(word) > 4){
+      if(strlen(word) >= 4){
         //printf("%d %s\n",strlen(word),word);
-        fprintf(output1,"%s ",word);
-        list = insertItem(list,word);
+        strcpy(array[i],word);
+        i++;
       }
       word = strtok (NULL, separator); //pega a pr�xima palavra do tweet
     }
   }
 
-  imprime(list,list,output2);
-	//lê o arquivo 'entrada' de tweets e retorna a árvore montada
+  *nItens = i;
 }
 
 void prtArrayCLI(int array[], int vectorLength){//Função prtArrayCLI. Printa a Array informada dentro do console. Utilizado em DEV.
@@ -75,67 +78,105 @@ FILE* initializeFileW(FILE* input, char* name){//Função initializeFileW. Inici
   return input;
 }
 
+void MSDRadixSort(char** aux, char** array, int low, int high, int index){
+  int i,j,r,p,t;
+
+  p=0;
+  if(high <= low){
+  }else{
+    int count[R+2];
+
+    for(i=0;i<R+2;i++){
+      count[i] = 0;
+    }
+
+    for(i=low;i <= high;i++){
+      p = (int)(array[i][index]);
+      p = p + 2;
+      count[p]++;
+    }
+
+    for(r=0;r<R+1;r++){
+      count[r+1] += count[r];
+    }
+
+    for(i=low;i <= high;i++){
+      p = (int)(array[i][index]) + 1;
+      t = count[p]++;
+      aux[t] = array[i];
+    }
+
+    for(i=low;i <= high;i++){
+      array[i] = aux[i-low];
+    }
+
+    for(r=0;r<R;r++){
+      MSDRadixSort(aux,array,low+count[r],low-1+count[r+1],index+1);
+    }
+  }
+}
+
+void sortB(char** array, int nItens){
+  char **aux = malloc(MAX_ITEMS * sizeof(char *));
+  int i;
+
+  for(i = 0; i < MAX_ITEMS; i++){
+    aux[i] = malloc(MAX_CHAR+1 * sizeof(char));
+  }//s [indice] [TAMANHCHAR];
+
+  MSDRadixSort(aux,array,0,nItens,0);
+}
+
 int main(){//Função main. Executa os testes requisitados.
   setlocale(LC_ALL,"");//Ajusta as configurações de print para console. Usado em DEV.
-  char buffer[MAX_CHAR][MAX_ITEMS];//Variáveis utilizadas nos testes.
   int nItems;//Quantifica quantos itens existem no array.
-  int i,j,k;//Índices de uso geral.
+  int i,j,k,t, totalRep;//Índices de uso geral.
   FILE* input;//Ponteiros para os arquivos.
-  FILE* output1,*output2;
+  FILE* output1;
+  char **array = malloc(MAX_ITEMS * sizeof(char *));
+  char *word;
+  char prevWord[MAX_CHAR];
+  char line[30];
+  char separator[]= {" —’0123456789,.&-‘*%%\?!;/-'@\"$#=~><()][}{:\b\r\a\7\\\"\'“”_"};
+
+  for(i = 0; i < MAX_ITEMS; i++){
+    array[i] = malloc(MAX_CHAR+1 * sizeof(char));
+  }//s [indice] [TAMANHCHAR];
 
   input = initializeFileR(input,DEFAULT_INPUT);//Inicializa os arquivos .txt
   output1 = initializeFileW(output1,DEFAULT_OUTPUT1);
-  output2 = initializeFileW(output2,DEFAULT_OUTPUT2);
 
-  indexWords(input, output1, output2, buffer);
-  /*
-  while(feof(input)==0){//Enquanto não encontra o fim do arquivo de entrada.
-    fscanf(input,"%d ",&nItems);//Resgata o número de itens no array.
-    for(i=0;i<nItems;i++){//Para cada item no array, armazena um número lido.
-      fscanf(input,"%d ",&buffer[i]);//Executa a leitura do .txt de entrada.
+  indexWords(input, array, &nItems);
+
+  sortB(array,nItems);
+
+  for(j=1; j < nItems+1; j++){
+    fprintf(output1,"%s\n",array[j]);
+  }
+  fclose(input);
+  fclose(output1);
+
+  input = initializeFileR(input,DEFAULT_OUTPUT1);//Inicializa os arquivos .txt
+  output1 = initializeFileW(output1,DEFAULT_OUTPUT2);
+
+  strcpy(prevWord,"--%%");
+
+  totalRep = 1;
+
+  while (fgets(line,MAX_CHAR+5,input)){
+    word = strtok(line, separator); //pega a 1a palavra do tweet
+    if(word == prevWord){
+      totalRep++;
+      fseek(output1,-(sizeof("%d\n")), SEEK_CUR );
+      fprintf(output1,"%d\n",array[j],totalRep);
+    }else{
+      totalRep = 1;
+      fprintf(output1,"%s #%d\n",word,totalRep);
+      strcpy(prevWord,word);
     }
+  }
 
-    for(j=0;j<4;j++){//Para cada array lido do .txt, executa as quatro variações do QuickSort.
-      for(k=0; k < nItems; k++) {//Copia o buffer para bufferClone.
-        bufferClone[k] = buffer[k];
-      }
-      swaps = 0;//Reseta variáveis de contagem.
-      recursions = 0;
-
-      switch(j){//Varia entres os tipos de QuickSort..
-        case 0:
-          t = clock();//Marca o início.
-          quickSortLomutoMedian(bufferClone,0,nItems-1,&swaps,&recursions);//Executa o ordenamento.
-          t = clock() - t;//Extrai o tempo total
-          cTime = ((double)t)/CLOCKS_PER_SEC;//Cálculo do tempo decorrido.
-          prtInfo(nItems,swaps,recursions,cTime,output2);//Printa as informações
-          break;
-        case 1:
-          t = clock();//Marca o início
-          quickSortLomutoRandom(bufferClone,0,nItems-1,&swaps,&recursions);//Executa o ordenamento.
-          t = clock() - t;//Extrai o tempo total
-          cTime = ((double)t)/CLOCKS_PER_SEC;//Cálculo do tempo decorrido.
-          prtInfo(nItems,swaps,recursions,cTime,output4);//Printa as informações
-          break;
-        case 2:
-          t = clock();//Marca o início
-          quickSortHoareMedian(bufferClone,0,nItems-1,&swaps,&recursions);//Executa o ordenamento.
-          t = clock() - t;//Extrai o tempo total
-          cTime = ((double)t)/CLOCKS_PER_SEC;//Cálculo do tempo decorrido.
-          prtInfo(nItems,swaps,recursions,cTime,output1);//Printa as informações
-          break;
-        case 3:
-          t = clock();//Marca o início
-          quickSortHoareRandom(bufferClone,0,nItems-1,&swaps,&recursions);//Executa o ordenamento.
-          t = clock() - t;//Extrai o tempo total
-          cTime = ((double)t)/CLOCKS_PER_SEC;//Cálculo do tempo decorrido.
-          prtInfo(nItems,swaps,recursions,cTime,output3);//Printa as informações
-          break;
-      }
-    }
-  }*/
   fclose(output1);//Fecha os ponteiros de leitura e escrita.
-  fclose(output2);
   fclose(input);
   return 0;
 }
